6842e2fc683804d921900f44cc526f2e0705abcf
ExoticBlade <74434340+ExoticBlade@users.noreply.github.com>
2024-02-08 15:55:17 -0600

    Update Random.py

---
+++ Random.py
@@ -0,0 +1,13 @@
+import random
+
+P = input("Do You Want To Generate a Random Number? \n")
+
+if P == "Yes" or "yes":
+	MinRange = int(input("Whats The Minimum Number? \n"))
+	MaxRange = int(input("Whats the Maximum Number? \n"))
+	Number = (random.randint(MinRange, MaxRange))
+	print("Your Number is:", Number)
+
+if P == "No" or "no":
+	exit()
+	
